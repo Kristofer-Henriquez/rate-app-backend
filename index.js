82f3102c5c9b50ec84e9dc142ab245fce0a556c3
Alan Barton <abarton201@gmail.com>
2021-01-08 18:54:23 -0500

    add put and delete roughts

--- index.js
+++ index.js
@@ -24,8 +24,56 @@ app.get('/api/reviews/:id', (req, res) => {
 });
 
 app.post('/api/reviews', (req, res) => {
-  const review = {
-    id: 
-    text:
+  const { error } = Joi.validate(req.body, schema);
+  if (error) {
+    res.status(400).send(error.detail[0].message);
+    return;
   }
+
+  const review = {
+    text: req.body.text,
+    professor: req.body.professor,
+  };
+  reviews.push(review);
+  res.send(review);
+});
+
+app.put('/api/reviews/:id', (req, res) => {
+  const review = reviews.find(r => r.id === parseInt(req.params.id));
+  if (!review) res.status(404).send('the review does not exist');
+
+  const result = validateReview(req.body)
+
+  const schema = {
+    name: Joi.string().min3.required(),
+    text: req.body.text,
+    professor: req.body.professor,
+  }
+
+  const { error } = Joi.validate(req.body, schema);
+  if (error) {
+    res.status(400).send(error.detail[0].message);
+    return;
+  }
+
+  review.name = req.body.name;
+  res.send(review);
+});
+
+function validateReview(review) {
+  const schema = {
+    name: Joi.string().min3.required(),
+  };
+
+  return Joi.validate(review, schema);
+}
+
+app.delete('/api/reviews/:id', (req, res) => {
+  const review = reviews.find(r => r.id === parseInt(req.params.id));
+  if (!review) res.status(404).send('the review does not exist');
+
+  const index = reviews.indexOf(review);
+  reviews.splice(index, 1);
+
+  res.send(reviews);
 });
