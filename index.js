1aff6e6ec870512805d61acb76f226bbea428879
Alan Barton <abarton201@gmail.com>
2021-01-08 21:52:11 -0500

    brocken sql

--- index.js
+++ index.js
@@ -1,79 +1,89 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const db = require('./queries');
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-const reviews = [
-  { id: 1, text: 'stuff1'},
-  { id: 2, text: 'stuff2'},
-  { id: 3, text: 'stuff3'},
-];
 
 app.listen(port, () => console.log(`listening on port ${port}...`));
 
-app.get('/api/reviews', (req, res) => {
-  res.send(reviews);
+// Professor Routes
+app.get('/professors', db.getProfessors);
+app.get('/professors/:id', db.getProfessorById);
+app.post('/professors', db.createProfessor);
+app.put('/professors/:id', db.updateProfessor);
+app.delete('/professors/:id', db.deleteProfessor);
+
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
-// may not be needed
-app.get('/api/reviews/:id', (req, res) => {
-  const review = reviews.find(r => r.id === parseInt(req.params.id));
-  if (!review) res.status(404).send('the review does not exist');
-  res.send(review);
-});
+// // may not be needed
+// app.get('/api/reviews/:id', (req, res) => {
+//   const review = reviews.find(r => r.id === parseInt(req.params.id));
+//   if (!review) res.status(404).send('the review does not exist');
+//   res.send(review);
+// });
 
-app.post('/api/reviews', (req, res) => {
-  const { error } = Joi.validate(req.body, schema);
-  if (error) {
-    res.status(400).send(error.detail[0].message);
-    return;
-  }
+// app.post('/api/reviews', (req, res) => {
+//   const { error } = Joi.validate(req.body, schema);
+//   if (error) {
+//     res.status(400).send(error.detail[0].message);
+//     return;
+//   }
 
-  const review = {
-    text: req.body.text,
-    professor: req.body.professor,
-  };
-  reviews.push(review);
-  res.send(review);
-});
+//   const review = {
+//     text: req.body.text,
+//     professor: req.body.professor,
+//   };
+//   reviews.push(review);
+//   res.send(review);
+// });
 
-app.put('/api/reviews/:id', (req, res) => {
-  const review = reviews.find(r => r.id === parseInt(req.params.id));
-  if (!review) res.status(404).send('the review does not exist');
+// app.put('/api/reviews/:id', (req, res) => {
+//   const review = reviews.find(r => r.id === parseInt(req.params.id));
+//   if (!review) res.status(404).send('the review does not exist');
 
-  const result = validateReview(req.body)
+//   const result = validateReview(req.body);
 
-  const schema = {
-    name: Joi.string().min3.required(),
-    text: req.body.text,
-    professor: req.body.professor,
-  }
+//   const schema = {
+//     name: Joi.string().min3.required(),
+//     text: req.body.text,
+//     professor: req.body.professor,
+//   }
 
-  const { error } = Joi.validate(req.body, schema);
-  if (error) {
-    res.status(400).send(error.detail[0].message);
-    return;
-  }
+//   const { error } = Joi.validate(req.body, schema);
+//   if (error) {
+//     res.status(400).send(error.detail[0].message);
+//     return;
+//   }
 
-  review.name = req.body.name;
-  res.send(review);
-});
+//   review.name = req.body.name;
+//   res.send(review);
+// });
 
-function validateReview(review) {
-  const schema = {
-    name: Joi.string().min3.required(),
-  };
+// function validateReview(review) {
+//   const schema = {
+//     name: Joi.string().min3.required(),
+//   };
 
-  return Joi.validate(review, schema);
-}
+//   return Joi.validate(review, schema);
+// }
 
-app.delete('/api/reviews/:id', (req, res) => {
-  const review = reviews.find(r => r.id === parseInt(req.params.id));
-  if (!review) res.status(404).send('the review does not exist');
+// app.delete('/api/reviews/:id', (req, res) => {
+//   const review = reviews.find(r => r.id === parseInt(req.params.id));
+//   if (!review) res.status(404).send('the review does not exist');
 
-  const index = reviews.indexOf(review);
-  reviews.splice(index, 1);
+//   const index = reviews.indexOf(review);
+//   reviews.splice(index, 1);
 
-  res.send(reviews);
-});
+//   res.send(reviews);
+// });
---
+++ queries.js
@@ -0,0 +1,79 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'professors',
+  password: 'password',
+  port: 5432,
+});
+
+// index
+const getProfessors = (request, response) => {
+  pool.query('SELECT * FROM professors ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+// show 
+const getProfessorById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+// create
+const createProfessor = (request, response) => {
+  const { name, title, school, department } = request.body;
+
+  pool.query('INSERT INTO users (name, title, school, department) VALUES ($1, $2, $3, $4)', [name, title, school, department], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`);
+  });
+};
+
+// update 
+const updateProfessor = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, title, school, department } = request.body;
+
+  pool.query(
+    'UPDATE professors SET name = $1, title = $2, school = $3, department = $4 WHERE id = $5',
+    [name, title, school, department, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+// delete 
+const deleteProfessor = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('DELETE FROM professors WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getProfessors,
+  getProfessorById,
+  createProfessor,
+  updateProfessor,
+  deleteProfessor,
+};
