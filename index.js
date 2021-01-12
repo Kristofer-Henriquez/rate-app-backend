dd008ff4cedff382a9daa8ddab7089934059ea92
Kristofer <kristoferhenriquez@gmail.com>
2021-01-11 20:42:24 -0600

    Added associations to Professors Show action

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 const db = require('./queries');
 
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+
 
 
 app.listen(port, () => console.log(`listening on port ${port}...`));
