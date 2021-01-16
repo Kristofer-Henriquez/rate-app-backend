894bd018a72a8d291751c790181aba21251dc428
Alan Barton <abarton201@gmail.com>
2021-01-16 16:54:24 -0500

    installing cors

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 const db = require('./queries');
+var cors = require('cors');
 
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'});
+});
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80');
+});
 
 app.use(bodyParser.json());
 app.use(
@@ -11,6 +20,7 @@ app.use(
     extended: true,
   })
 );
+app.use(cors());
 
 
 
