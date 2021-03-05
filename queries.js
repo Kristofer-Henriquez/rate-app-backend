0f4cdf136a7a18dbac6c746b7ee0e7d0257fb1fb
Kristofer <kristoferhenriquez@gmail.com>
2021-03-04 19:51:55 -0600

    modified connection string in quereies.js

--- queries.js
+++ queries.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 
 const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === 'production';
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = isProduction 
+  ? process.env.DATABASE_URL 
+  : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Professors
